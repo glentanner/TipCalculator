51c1a75b428042150d1249faed723b5caa4692b7
Glen Tanner <grtanner@gmail.com>
2022-06-07 15:21:47 -0600

    Min tip amount, dynamic slider, round up, change color

--- app/src/main/java/com/grtapplications/android/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/grtapplications/android/tipcalculator/MainActivity.kt
@@ -6,32 +6,19 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.grtapplications.android.tipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
+import kotlin.math.roundToInt
+
 
 class MainActivity : AppCompatActivity() {
 
-    /*
-    To add binding:
-        1. Add block to Module gradle.build file:
-               buildFeatures { viewBinding = true }
-        2. Add variable
-               lateinit var binding: ActivityMainBinding
-        3. Add lines to onCreate()
-               binding = ActivityMainBinding.inflate(layoutInflater)
-               setContentView(binding.root)
-        // Old way with findViewById()
-        val myButton: Button = findViewById(R.id.my_button)
-        myButton.text = "A button"
-
-        // Better way with view binding
-        val myButton: Button = binding.myButton
-        myButton.text = "A button"
-
-        // Best way with view binding and no extra variable
-        binding.myButton.text = "A button"
-     */
     private lateinit var binding: ActivityMainBinding
+    private var splitOption: Int = 1
+    private val minCost: Double = 2.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,29 +32,66 @@ class MainActivity : AppCompatActivity() {
         binding.costOfService.setOnKeyListener { view, keyCode, _ ->
             handleKeyEvent(view, keyCode)
         }
-
+        // Listen for Split Slider value
+        val splitSlider: Slider = binding.splitSlider
+        splitSlider.addOnChangeListener { _, value, _ ->
+             splitOption = value.roundToInt()
+            if (binding.totalResult.text.isNotEmpty()) {
+                calculateTip()
+            }
+        }
+        // Listen for Switch
+        val roundupSwitch: SwitchMaterial = binding.roundUpSwitch
+        roundupSwitch.setOnCheckedChangeListener { _, _ ->
+            if (binding.totalResult.text.isNotEmpty()) {
+                calculateTip()
+            }
+        }
+        // On startup, focus on Cost TextField
+        binding.costOfService.requestFocus()
     }
 
     private fun calculateTip() {
         // Get the cost of service as text, convert the string to a double (decimal)
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
+
         if (cost == null) {
+            val snack = Snackbar.make(binding.root,"Enter an amount to calculate tip",Snackbar.LENGTH_LONG)
+            snack.setAction("DISMISS", View.OnClickListener {
+            })
+            snack.show()
             binding.tipResult.text = ""
             binding.totalResult.text = ""
+            binding.numChecks.text = ""
             return
         }
 
         // Get tip percentage from the selected radio button
         val tipPercentage =
             when (binding.tipOptions.checkedRadioButtonId) {
+                R.id.option_twenty_five_percent -> 0.25
                 R.id.option_twenty_percent -> 0.20
                 R.id.option_eighteen_percent -> 0.18
-                R.id.option_fifteen_percent -> 0.15
                 else -> 0.15
             }
+
         // Calculate tip
-        var tip = tipPercentage * cost
+        if (cost * tipPercentage < minCost) {
+            val snack = Snackbar.make(binding.root,"Be nice. Minimal tip is $2",Snackbar.LENGTH_LONG)
+            snack.setAction("DISMISS", View.OnClickListener {
+                binding.costOfService.text!!.clear()
+            })
+            snack.show()
+            binding.tipResult.text = ""
+            binding.totalResult.text = ""
+            binding.numChecks.text = ""
+            return
+        }
+
+        var tip = (tipPercentage * cost) / splitOption
+        val total = (cost / splitOption) + tip
+
         // Round up if desired
         if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
@@ -75,10 +99,11 @@ class MainActivity : AppCompatActivity() {
 
         // Format currency according to user settings
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        val formattedTotal = NumberFormat.getCurrencyInstance().format(cost + tip)
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
 
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
         binding.totalResult.text = getString(R.string.total_amount, formattedTotal)
+        binding.numChecks.text = getString(R.string.num_checks, splitOption.toString())
     }
 
     private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
